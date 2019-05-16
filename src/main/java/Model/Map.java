ffce804f81af4cde721366c6d019806f1ddfc3fb
Bryan Andriamasy <bryan.andriamasy@epita.fr>
2019-05-16 18:03:00 +0200

    merge UserInterface

--- src/main/java/Model/Map.java
+++ src/main/java/Model/Map.java
@@ -1,10 +1,22 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Observable;
 
-public class Map {
+public class Map extends Observable {
     private String name;
     private int width;
     private int height;
-    private ArrayList<Map> tiles;
+    private ArrayList<Tile> tiles;
+
+    public void add_foreground_object(String type) {
+        //Definitely not the final code
+        this.name = type;
+        setChanged();
+        notifyObservers();
+    }
+
+    public String get_name() {
+        return name;
+    }
 }
