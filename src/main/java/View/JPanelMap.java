c3bac004afc8946b542c44fcc7773fa8fefca226
Tristan Molter <tristan.molter@epita.fr>
2019-05-19 17:05:32 +0200

    Made a grid in the panel for the maps

--- src/main/java/View/JPanelMap.java
+++ src/main/java/View/JPanelMap.java
@@ -19,12 +19,32 @@ public class JPanelMap extends JPanel{
         if (panelImage != null) {
             g.drawImage(panelImage, 0, 0, null);
         }
+        printGrid(this.getWidth(), this.getHeight(), g);
     }
 
     public void addTile(Image im) {
         Graphics g = panelImage.getGraphics();
         g.drawImage(im, 0, 0, null);
-        g.drawImage(im, 20, 20, null);
+        g.drawImage(im, 16, 16, null);
+        g.dispose();
+    }
+
+
+    public void printGrid(int width, int height, Graphics g)
+    {
+        //Graphics g = panelImage.getGraphics();
+        //Graphics g = this.getGraphics();
+        for (int i = 0; i * 16 < height; i++)
+        {
+            g.setColor(Color.BLACK);
+            g.drawLine(0, i * 16, width, i * 16);
+        }
+
+        for (int i = 0; i * 16 < width; i++)
+        {
+            g.setColor(Color.BLACK);
+            g.drawLine(i * 16, 0, i * 16, height);
+        }
         g.dispose();
     }
 }
