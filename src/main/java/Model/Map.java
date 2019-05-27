eeb990394fa23374ef6ad2074e3ee22be7cc2855
Bryan Andriamasy <bryan.andriamasy@epita.fr>
2019-05-27 17:26:03 +0200

    Foreground object cannot be stacked

--- src/main/java/Model/Map.java
+++ src/main/java/Model/Map.java
@@ -17,22 +17,32 @@ public class Map {
     public void initTiles() {
         for(int i = 0; i < width; ++i) {
             for (int j = 0; j < height; ++j) {
-                tiles[i][j] = new Tile(false, true, false, null, null);
+                tiles[i][j] = new Tile(true, true, false, null, null);
             }
         }
     }
 
     public void addBackground(Type type, int x, int y) {
         tiles[x][y].setBackground(type);
-        tiles[x][y].setWalkable(true);
-        tiles[x][y].setEmpty(true);
-        tiles[x][y].setBegin(false);
+        //tiles[x][y].setWalkable(true);
+        //tiles[x][y].setEmpty(true);
+        //tiles[x][y].setBegin(false);
+    }
+
+    public boolean isNotEmpty(int x, int y, int w, int h) {
+        for (int i = 0; i < w; ++i) {
+            for (int j = 0; j < h; ++j) {
+                if (!tiles[x + i][y + j].isEmpty())
+                    return true;
+            }
+        }
+        return false;
     }
 
     public void addForeground(Type type, int x, int y) {
         int w = type.getWidth();
         int h = type.getHeight();
-        if (width < x + w || height < y + h)
+        if (width < x + w || height < y + h || isNotEmpty(x, y, w, h))
             return;
         for (int i = 0; i < w; ++i) {
             for (int j = 0; j < h; ++j) {
